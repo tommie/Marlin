c5975efcf9e239d67ded16e9c6c7ff410a80afcb
Tommie Gannert <tommie@gannert.se>
2018-01-07 15:19:32 +0000

    Creality Ender support.
    Reverse engineered from the unpublished firmware from Creality,
    inferring the base version and configuration they used. The basis is
    the firmware version from "Jul 31 2017 10:16:30". Configurations were
    found by seeing what code was compiled into the firmware, and
    constants used there.
    
    They used Marlin 1.0.1, because
    
    * 1.0.0 had very different serial output in `setup()` and overall
      code structure.
    * 1.0.2 changed the `VERSION_STRING` to include a leading space,
      and `lcd_init` uses `SET_INPUT` instead of `pinMode`.
    
    For U8Glib, a version between 1.14 and 1.17 was used, because
    
    * 1.12 didn't have the extra speed argument to u8g_InitCom.
    * 1.13 didn't have the soft reset instruction for UC1701 initialization.
    * 1.18 has a new directory structure.
    
    Quirks
    
    * The value of PID_dT hints that F_CPU is 20M, but MarlinSerial.begin
      suggests it's indeed 16M (and the board uses 16M). Left at 16M for now.
    
    * The LED and DOGLCD_CS are on the same pin.

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -194,6 +194,8 @@
   #include "pins_MELZI_MAKR3D.h"      // ATmega644P, ATmega1284P
 #elif MB(MELZI_CREALITY)
   #include "pins_MELZI_CREALITY.h"    // ATmega644P, ATmega1284P
+#elif MB(CREALITY_ENDER)
+  #include "pins_CREALITY_ENDER.h"    // ATmega1284P
 #elif MB(STB_11)
   #include "pins_STB_11.h"            // ATmega644P, ATmega1284P
 #elif MB(AZTEEG_X1)
